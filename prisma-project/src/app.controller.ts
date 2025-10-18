5bcfc3376ea5103850d2087a8ff914a9afa1d3de
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-10-19 02:20:36 +0300

    feat: reading env variables

--- prisma-project/src/app.controller.ts
+++ prisma-project/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+console.log('test env var', process.env.TEST);
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
